e6d6d5c270d52688b14e78ce8359e4685aa5fba1
Sourav Kanta <sourav.kanta@gmail.com>
2020-04-17 21:39:42 +0530

    second commit

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-app.listen(8080);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/api', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
